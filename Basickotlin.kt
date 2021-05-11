0359723f7fae75c68e62196682340ef2c0c50f2b
ThisIsRameshMishra <40620498+ThisIsRameshMishra@users.noreply.github.com>
2021-05-11 14:38:11 +0530

    Create Basickotlin.kt

---
+++ Basickotlin.kt
@@ -0,0 +1,169 @@
+fun main() {
+//     println("Hello, world!!!")
+	
+//     var x:Int=5
+//     val y:Int=10
+//     x=20
+//    // y=25(cant reaasigned)
+//    var name:String="Anuj"
+//     println(""+y+""+"name is $name"+x)
+// 		var myname:String?=null
+// 		
+// 		
+//     if(myname==null){
+//         print(myname)
+//     }
+//     
+//     
+// 	val age=25
+//     if(age==24){
+//         print("shfs")
+//     }
+//     else{
+//         print("sdfkhsl")
+//     }
+//     
+//     
+// 		val value:Int=if(true){
+//             print("inside true")
+//             10
+//         }else{
+//             20
+//         }
+//         print(value)
+//         
+//         
+// //         
+// 			val name="ramesh"
+//    val x= when(name){
+//         "ramesh"->{
+//             print("Awesome")
+//         }
+//         else ->{
+//             print("sahi hai")
+//         }
+       
+//     }
+//     print(x)
+    
+//     A r r a y s
+//     	val names=arrayOf("apple","bannana","milk")
+//     	for(name in names){
+//             println(name+" ")
+//         }
+//         names.forEach{
+//             println(it+" ")
+//         }
+        //
+        //
+        //      L O O P S
+        //
+        //
+        //
+//   		for(i in 0..3){
+//             print(i)
+//         }
+//         for(i in 2..8 step 2){
+//             print(i)
+//         }
+//         for(i in 3 downTo 0){
+//             print(i)
+//         }
+//         val x=7
+//     	if(x in 1..5){
+//             println("x is present")
+//         }
+//         else{
+//             println("not present")
+//         }
+//        
+//        
+//        
+//        
+//        L I S T S
+//        
+//        
+//        
+//        
+       
+//     	val names=listOf("apple","bannana","milk")
+//         val namess=mutableListOf<String>("apple","bannana","milk","1")
+//        namess[0]="khao"
+//     println(namess[0])
+//     	for(name in namess){
+//             println(name)
+//         }
+      //  namess.forEach{
+        //    println(it+" ")
+        //}
+        
+    
+    
+    
+    
+    // FUNCTIONS
+    // 
+    // 
+    // 
+    // 
+    // 
+    // 
+//     println(operation(2,5))
+    
+//     greetings(name="Ramesh",greet="Good evening")
+//     
+//     
+//     H I G H E R      O R D E R     F U N C T I O N S
+    
+//     	val output=operation(4,5,{a,b->
+//             print("adding these 2 numbers")
+//            val temp=a*a
+//              temp+b
+//         })
+//         print(output)
+//         E    N       D
+//         
+//         
+//         
+//         O       O     P       S
+//         
+//         val p1=person("Ramesh")
+//         print(p1.name)
+//         
+}
+//     
+//     
+//     
+//     O      O       P       S
+//     
+//     
+    
+//     class person(val name:String) 
+       
+   
+    
+    
+    
+    
+    
+    
+    
+    
+// H I G H E R      O R D E R     F U N C T I O N S
+	//void->unit
+// 	fun operation(a:Int,b:Int,operate:(Int,Int)->Int):Int{
+//         return operate(a,b)
+//     }
+//     E       N         D
+
+
+
+
+
+//     fun operation(a:Int,b:Int):Int{
+//         return a+b
+//     }
+//      fun operation(a:Int,b:Int):Int=a+b
+//     fun greetings(greet: String="good night",name:String){
+//         print("hello $name,$greet")
+//     }
